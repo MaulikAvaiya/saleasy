da0fd6766c45c263f1e9907745139dae19accb39
Maulik avaiya <avaiyamaulik8@gmail.com>
2022-01-04 20:51:21 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'myapp',
+      theme: ThemeData(
+        accentColor: Colors.amber,
+        primaryColor: Colors.indigo,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('home'),
+      ),
+      body: Center(
+        child: Text('this my home page'),
+      ),
+    );
+  }
+}
