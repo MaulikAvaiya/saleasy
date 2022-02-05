f1289851e9a931f99e2511c2f9d7556e61e4c7fa
Maulik avaiya <avaiyamaulik8@gmail.com>
2022-02-05 15:54:26 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SalEasy',
+      title: 'SalEasy App',
       theme: ThemeData(
        primaryColor: Colors.tealAccent[400],
         
